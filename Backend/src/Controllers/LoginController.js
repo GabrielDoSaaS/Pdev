f82f796285968eb4493dba4020c72960e70c8571
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-08-02 14:32:18 -0300

    Criação da Rota + Controlador de Login

---
+++ Backend/src/Controllers/LoginController.js
@@ -0,0 +1,39 @@
+const User = require('../Models/User');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
+
+const LoginController = () => {
+  const login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+      return next(new AppError('Por favor, forneça e-mail e senha', 400));
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('E-mail ou senha incorretos', 401));
+    }
+
+    const token = user.createAuthToken();
+
+    user.password = undefined;
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Login realizado com sucesso!',
+      token,
+      data: {
+        user
+      }
+    });
+  });
+
+  return {
+    login
+  };
+};
+
+module.exports = LoginController;
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router( );
 const GitHubFunctionsController = require('../Controllers/GitHubFunctionsController');
+const LoginController = require('../Controllers/LoginController');
 const RegisterController = require('../Controllers/RegisterController');
 
 routes.post('/api/createRepository', GitHubFunctionsController.CreateRepositoryInGithub)
 routes.post('/api/register', RegisterController);
+routes.post('/api/login', LoginController);
 
 module.exports = routes;
