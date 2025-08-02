d47cc22cf654589f453cb9b23b2a92eca0a5981e
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-08-01 21:24:55 -0300

    Criação da rota de criação de repositório
    Criei o controlador responsável pela criação automatizada de repositórios públicos utilizando a API do GitHub utilizando Axios para requisição. Essa etapa é necessária para a futura integração com a API da Render.com.

---
+++ Backend/src/Controllers/GitHubFunctionsController.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const githubToken = process.env.GITHUB_TOKEN;
+
+
+if (!githubToken) {
+    console.error('Erro: O token do GitHub não foi encontrado. Verifique seu arquivo .env.');
+    process.exit(1);
+}
+
+const apiUrl = 'https://api.github.com/user/repos';
+
+const config = {
+    headers: {
+        'Authorization': `token ${githubToken}`,
+        'Content-Type': 'application/json'
+    }
+};
+
+const CreateRepositoryInGithub = async ( req, res ) => {
+    const {repoName, description} = req.body;
+    
+    const data = {
+    name: repoName,
+    description: description,
+    private: false
+    };
+
+    try {
+        const response = await axios.post(apiUrl, data, config);
+        console.log(`Repositório '${response.data.name}' criado com sucesso!`);
+        console.log(`URL: ${response.data.html_url}`);
+    } catch (error) {
+        if (error.response) {
+            console.error('Erro ao criar o repositório:', error.response.data.message);
+        } else if (error.request) {
+            console.error('Erro: Nenhuma resposta recebida do GitHub.');
+        } else {
+            console.error('Erro:', error.message);
+        }
+    }
+}
+
+module.exports = {
+    CreateRepositoryInGithub: CreateRepositoryInGithub
+}
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -1,4 +1,7 @@
 const routes = require('express').Router( );
+const GitHubFunctionsController = require('../Controllers/GitHubFunctionsController');
+
+routes.post('/api/createRepository', GitHubFunctionsController.CreateRepositoryInGithub)
 
 
 module.exports = routes;
