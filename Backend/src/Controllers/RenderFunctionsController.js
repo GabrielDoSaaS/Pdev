54d048e387a65b9f252388fd5dec22e522647a6a
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-08-02 19:44:33 -0300

    Criação de controlador e rota para Render API

---
+++ Backend/src/Controllers/RenderFunctionsController.js
@@ -0,0 +1,40 @@
+const CreateStaticSite = async ( req, res ) => {
+    const {repositoryName, repoUrl, branch} = req.body;
+
+
+    if (!process.env.RENDER_API_KEY) {
+        console.error('RENDER_API_KEY não está definida nas variáveis de ambiente.');
+        res.status(401);
+        return;
+    }
+
+    const payload = {
+        type: 'static_site',
+        name: repositoryName,
+        ownerId: process.env.OWNER_ID,
+        repo: repoUrl,
+        branch: branch,
+        staticSiteDetails: {
+        buildCommand: '',
+        publishPath: '.',
+        },
+    };
+
+    try {
+        const response = await axios.post('https://api.render.com/v1/services', payload, {
+            headers: {
+            'Authorization': `Bearer ${process.env.RENDER_API_KEY}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    console.log('Site estático criado com sucesso!');
+    console.log('Detalhes do serviço:', response.data);
+    res.send(response.data);
+  } catch (error) {
+    console.error('Erro ao criar o site estático:', error.response ? error.response.data : error.message);
+    res.status(500);
+  }
+};
+
+module.exports = {CreateStaticSite: CreateStaticSite};
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -2,9 +2,11 @@ const routes = require('express').Router( );
 const GitHubFunctionsController = require('../Controllers/GitHubFunctionsController');
 const LoginController = require('../Controllers/LoginController');
 const RegisterController = require('../Controllers/RegisterController');
+const RenderFunctionsController = require('../Controllers/RenderFunctionsController');
 
 routes.post('/api/createRepository', GitHubFunctionsController.CreateRepositoryInGithub)
 routes.post('/api/register', RegisterController);
 routes.post('/api/login', LoginController);
+routes.post('/api/createStaticProject', RenderFunctionsController.CreateStaticSite);
 
 module.exports = routes;
