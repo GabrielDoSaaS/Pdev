11acd9c554f404dbfbc404cd51914d977f6fb240
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-08-03 12:10:15 -0300

    Criação de controlador para criação de pagamentos

---
+++ Backend/src/Controllers/StripeController.js
@@ -0,0 +1,23 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const CreatePayment = async (req, res) => {
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 10.0, 
+            currency: 'usd', 
+            payment_method_types: ['card']
+        })
+
+        res.status(200).json({
+            clientSecret: paymentIntent.client_secret,
+        });
+
+    } catch (error) {
+        console.error('Erro ao criar o PaymentIntent:', error);
+        res.status(500).json({
+            error: 'Ocorreu um erro no servidor.',
+        });
+    }
+};
+
+module.exports = {CreatePayment: CreatePayment};
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -3,10 +3,12 @@ const GitHubFunctionsController = require('../Controllers/GitHubFunctionsControl
 const LoginController = require('../Controllers/LoginController');
 const RegisterController = require('../Controllers/RegisterController');
 const RenderFunctionsController = require('../Controllers/RenderFunctionsController');
+const StripeController = require('../Controllers/StripeController');
 
 routes.post('/api/createRepository', GitHubFunctionsController.CreateRepositoryInGithub);
 routes.post('/api/register', RegisterController);
 routes.post('/api/login', LoginController);
 routes.post('/api/createStaticProject', RenderFunctionsController.CreateStaticSite);
+routes.post('/api/create-payment', StripeController.CreatePayment);
 
 module.exports = routes;
